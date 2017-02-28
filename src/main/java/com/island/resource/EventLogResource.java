d530f21c3dea9ac21e13cbc82df0a02861fde07d
jackyoh <jack@is-land.com.tw>
2017-02-28 11:42:44 +0800

    The resource add the log information

--- src/main/java/com/island/resource/EventLogResource.java
+++ src/main/java/com/island/resource/EventLogResource.java
@@ -12,13 +12,16 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.Gson;
 import com.island.bean.TaskInfoBean;
 import com.island.util.Constants;
 
 @Path("v2")
 public class EventLogResource {
-	
+	private static Logger LOGGER = LoggerFactory.getLogger(EventLogResource.class);
 	@Context
 	private ServletContext context;
 	   
@@ -26,10 +29,12 @@ public class EventLogResource {
 	@Path("apps")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String lists(){
+		LOGGER.info("List application");
 		Map<String, TaskInfoBean> statusCache = 
                 (Map<String, TaskInfoBean>) context.getAttribute(Constants.STATUSCACHE);
 		Gson gson = new Gson();
 		if(statusCache == null){
+			LOGGER.info("Not application in the list");
 			return "";
 		}
 		return gson.toJson(statusCache, Map.class);
@@ -41,14 +46,16 @@ public class EventLogResource {
 	public String appStatus(@PathParam("appId") String appId){
 		Map<String, TaskInfoBean> statusCache = 
                 (Map<String, TaskInfoBean>) context.getAttribute(Constants.STATUSCACHE);
-		
+		LOGGER.info("Query " + appId + " application");
 		Gson gson = new Gson();
 		
 		if(statusCache == null){
+			LOGGER.info("Not application in the list");
 			return "";
 		}
 		TaskInfoBean taskStatus = statusCache.get("/" + appId);
 		if(taskStatus == null){
+			LOGGER.info(appId + " application is not found");
 			return "";
 		}
 		return gson.toJson(taskStatus, TaskInfoBean.class);
@@ -58,12 +65,14 @@ public class EventLogResource {
 	@Path("taskstatus")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String tasksStatus(){
+		LOGGER.info("List tasks status");
 		Map<String, TaskInfoBean> statusCache = 
                 (Map<String, TaskInfoBean>) context.getAttribute(Constants.STATUSCACHE);
 		
 		Gson gson = new Gson();
 		
 		if(statusCache == null){
+			LOGGER.info("Not application in the list");
 			return "";
 		}
 		Map<String, String> maps = new HashMap<String, String>();
@@ -77,17 +86,20 @@ public class EventLogResource {
 	@Path("taskstatus/{appId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String taskStatus(@PathParam("appId") String appId){
+		LOGGER.info("Query " + appId + " application status");
 		Map<String, TaskInfoBean> statusCache = 
                 (Map<String, TaskInfoBean>) context.getAttribute(Constants.STATUSCACHE);
 		
 		Gson gson = new Gson();
 		
 		if(statusCache == null){
+			LOGGER.info("Not application in the list");
 			return "";
 		}
 		
 		TaskInfoBean taskStatus = statusCache.get("/" + appId);
 		if(taskStatus == null){
+			LOGGER.info(appId + " application status is not found");
 			return "";
 		}
 		return gson.toJson(taskStatus.getTaskStatus(), String.class);
